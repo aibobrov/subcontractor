2a6e0cc2c241580e77eb40d768a59f1376b8f7bf
Artem Bobrov <artembobrov3059@gmail.com>
2020-08-04 17:48:57 +0200

    [WIP][refactor]: remove extra code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,6 @@
 import org.springframework.boot.runApplication
-import slack.service.SlackRequestProvider
-import slack.service.SlackRequestManagerProviderImpl
 import slack.server.SlackApplication
 
-
-val provider: SlackRequestProvider = SlackRequestManagerProviderImpl()
 fun main(args: Array<String>) {
     runApplication<SlackApplication>(*args)
 }
-//fun main() {
-//    runApplication<SlackApplication>()
-//    val app = App()
-//
-
-//
-//    app.blockAction("single-option-poll-add") { req, context ->
-//        context.client().viewsPush { requestBuilder ->
-//            val tmp = AddOptionsPollView(MOCK.OPTIONS.map { it.content.toString() })
-//
-//            requestBuilder
-//                .view(tmp.representation())
-//                .triggerId(context.triggerId)
-//        }
-//        context.ack()
-//    }
-
-// don't forget to run `ngrok http 3000` and change urls in slack api
-//    val server = SlackAppServer(app) // starts server at 3000 port
-//    server.start()
-//}
