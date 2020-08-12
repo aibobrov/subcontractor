666d43849924b3a6a204dea7d958129c356d9bd6
Liza <liza8361@yandex.ru>
2020-08-12 18:58:08 +0300

    fix

--- subcontractor-core/src/main/kotlin/core/logic/DataStorage.kt
+++ subcontractor-core/src/main/kotlin/core/logic/DataStorage.kt
@@ -1,12 +1,12 @@
 package core.logic
 
 interface DataStorage {
-    fun addOrder(orderId: OrderId, userId: UserId, order: Order) : DispatcherResponse
-    fun deleteOrder(orderId: OrderId) :DispatcherResponse
+    fun addOrder(orderId: OrderId, userId: UserId, order: Order) : DispatcherError
+    fun deleteOrder(orderId: OrderId) : DispatcherError
     fun getOrder(orderId: OrderId) : Order?
-    fun addNode(orderId: OrderId, node : Node) : DispatcherResponse
-    fun modifyNode(orderId: OrderId, node : Node) : DispatcherResponse
-    fun deleteNode(orderId: OrderId, userId: UserId) : DispatcherResponse
+    fun addNode(orderId: OrderId, node : Node) : DispatcherError
+    fun modifyNode(orderId: OrderId, node : Node) : DispatcherError
+    fun deleteNode(orderId: OrderId, userId: UserId) : DispatcherError
     fun getNode(orderId: OrderId, userId: UserId) : Node?
     fun getCustomer(orderId: OrderId) : UserId?
 }
