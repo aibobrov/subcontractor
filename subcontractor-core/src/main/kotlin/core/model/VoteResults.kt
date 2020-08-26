62befeb18c51ebb43a1ce961ff30b0e40ac6649c
Artem Bobrov <artembobrov3059@gmail.com>
2020-08-26 20:11:17 +0200

    [refactor]: redundant code

--- subcontractor-core/src/main/kotlin/core/model/VoteResults.kt
+++ subcontractor-core/src/main/kotlin/core/model/VoteResults.kt
@@ -4,13 +4,4 @@ import core.model.base.OptionID
 
 data class VoteResults(val results: MutableMap<OptionID, MutableList<Voter>>) : Map<OptionID, List<Voter>> by results {
     val totalVoters: Int = results.values.fold(0) { acc, list -> acc + list.size }
-
-    companion object {
-        val EMPTY = VoteResults(mutableMapOf())
-        /* fun empty(options: List<PollOption>): VoteResults {
-             return VoteResults(
-                 options.map { it.id to setOf<Voter>() }.toMap()
-             )
-        }*/
-    }
 }
